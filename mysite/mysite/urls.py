1a860dde9f93f88759a6992708433826dfb53404
mblosiu <mblosiu@interia.pl>
2020-04-18 12:33:01 +0200

    pages init

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import homepage
+
 urlpatterns = [
+    path('', homepage),
     path('admin/', admin.site.urls),
 ]
