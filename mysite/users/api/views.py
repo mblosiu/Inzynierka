ae81637e6932ea0ac8212503589506ceb7ec4103
mblosiu@interia.pl <3ky6nuRU>
2020-06-06 15:39:41 +0200

    test github acc

--- mysite/users/api/views.py
+++ mysite/users/api/views.py
@@ -121,7 +121,7 @@ class UsersList(generics.ListCreateAPIView):
         return Response(serializer.data)
 
 # TODO: filters
-
+# x
 @permission_classes([IsAuthenticated])
 class UsersListFilter(generics.ListCreateAPIView):
     serializer_class = UserSerializer
