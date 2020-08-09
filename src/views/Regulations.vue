be7ab0e2679a77410323f89324b7b1e37535b8bb
mblosiu@interia.pl <mblosiu@interia.pl>
2020-08-09 11:36:38 +0200

    contact and regulations pages

---
+++ src/views/Regulations.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="regulations">
+    <h1>This is an regulations page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Regulations',
+  components: {
+    
+  }
+}
+</script>
