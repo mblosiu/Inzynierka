93ecd1dd85d39434bbd2ef60e204cd9a03ca23ec
mblosiu <mblosiu@interia.pl>
2021-01-18 13:04:46 +0100

    chat - chat new functions

--- Backend/chat/api/urls.py
+++ Backend/chat/api/urls.py
@@ -5,5 +5,8 @@ from .views import ConversationView
 app_name = 'chat'
 
 urlpatterns = [
-    path('<str:username>', ConversationView.as_view(), name="conversation"),
+    path('<str:username>/send-msg', ConversationView.as_view({'post': 'create_msg'}), name="send-msg"),
+    path('<str:username>/get-all-msgs', ConversationView.as_view({'get': 'get_msgs'}), name="conversation"),
+    path('<str:username>/get-last-x-msgs', ConversationView.as_view({'get': 'get_last_x'}), name="conversation"),
+    path('<str:username>/get-new-msgs', ConversationView.as_view({'get': 'get_new'}), name="conversation"),
 ]
--- Backend/chat/api/views.py
+++ Backend/chat/api/views.py
@@ -22,9 +22,9 @@ from .serializers import MessageSerializer
 
 
 @permission_classes([IsAuthenticated])
-class ConversationView(APIView):
+class ConversationView(viewsets.GenericViewSet):
     @staticmethod
-    def post(request, username):
+    def create_msg(request, username):
         sender = request.user
         receiver = get_object_or_404(User, username=username)
         text_message = request.data.get("message")
@@ -41,7 +41,7 @@ class ConversationView(APIView):
         return Response({"detail": "message sent successfully"}, status=status.HTTP_201_CREATED)
 
     @staticmethod
-    def get(request, username):
+    def get_msgs(request, username):
         user = request.user
         interlocutor = get_object_or_404(User, username=username)
         if request.user.username == username:
@@ -55,6 +55,51 @@ class ConversationView(APIView):
         # response['interlocutor_messages'] = interlocutor_messages_serializer.data
 
         result_list = sorted(chain(user_messages, interlocutor_messages), key=attrgetter('created'))
+        list_serializer = MessageSerializer(result_list, many=True)
+
+        return Response({'detail': list_serializer.data}, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def get_last_x(request, username):
+        user = request.user
+        interlocutor = get_object_or_404(User, username=username)
+        if request.user.username == username:
+            return Response({"detail": "you can't chat with yourself"}, status=status.HTTP_400_BAD_REQUEST)
+
+        x = request.data.get("x")
+
+        user_messages = Message.objects.filter(sender=user, receiver=interlocutor)
+        interlocutor_messages = Message.objects.filter(sender=interlocutor, receiver=user)
+
+        result_list = sorted(chain(user_messages, interlocutor_messages), key=attrgetter('created'), reverse=False)
+
+        if x is None or x == '':
+            x = 20
+
+        if not x.isdecimal():
+            x = 20
+
+        x = int(x)
+        if x > 1:
+            result_list = result_list[-x:]
+        list_serializer = MessageSerializer(result_list, many=True)
+
+        return Response({'detail': list_serializer.data}, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def get_new(request, username):
+        user = request.user
+        interlocutor = get_object_or_404(User, username=username)
+
+        last_date = request.data.get("last_date")
+
+        if request.user.username == username:
+            return Response({"detail": "you can't chat with yourself"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user_messages = Message.objects.filter(sender=user, receiver=interlocutor, created__gt=last_date)
+        interlocutor_messages = Message.objects.filter(sender=interlocutor, receiver=user, created__gt=last_date)
+
+        result_list = sorted(chain(user_messages, interlocutor_messages), key=attrgetter('created'), reverse=False)
         list_serializer = MessageSerializer(result_list, many=True)
 
         return Response({'detail': list_serializer.data}, status=status.HTTP_200_OK)
