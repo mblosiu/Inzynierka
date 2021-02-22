b62b617c8e6df791da1f5215a1d60e0634994c78
mblosiu <mblosiu@interia.pl>
2021-02-22 06:31:02 +0100

    unit tests

--- Backend/users/tests.py
+++ Backend/users/tests.py
@@ -6,21 +6,42 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 from rest_framework.test import force_authenticate
+from rest_framework.authtoken.models import Token
 
-from .api.urls import user_detail, user_list
-from .api.views import RegistrationView, UserProfileView, PreferencesView, DeleteUserAccountView, UserProfilePic
-from .models import User
+from users.api.urls import user_detail, user_list
+# from .api.views import CustomAuthToken
+from users.api.views import RegistrationView, UserProfileView, PreferencesView, SettingsView, DeleteUserAccountView, \
+    UserProfilePic, UserListView, CustomAuthToken, LogoutView, LikesView, BlackListView, FriendListView
+from users.models import User, Preferences, Settings
+from users.api.urls import user_detail, user_list, create_like, delete_like, get_liked_users, get_like_users, \
+    get_liked_user, get_like_user
 
 
 # Create your tests here.
 
-class AuthenticationTestCase(APITestCase):
+class UsersTestCase(APITestCase):
     def setUp(self):
-        User.objects.create_user(username="testcase1", email="testcase1@gmail.com", location="Poznań",
-                                 birthday="1996-06-20",
-                                 sex="female", password="password")
+        User1 = User.objects.create_user(username="testcase1", email="testcase1@gmail.com", location="Poznań",
+                                         birthday="1996-06-20",
+                                         sex="female", password="password")
+        User2 = User.objects.create_user(username="testcase2", email="testcase2@gmail.com", location="Poznań",
+                                         birthday="1996-06-20",
+                                         sex="female", password="password")
+        User3 = User.objects.create_user(username="testcase3", email="testcase3@gmail.com", location="Poznań",
+                                         birthday="1996-06-20",
+                                         sex="female", password="password")
+        User4 = User.objects.create_user(username="testcase4", email="testcase4@gmail.com", location="Poznań",
+                                         birthday="1996-06-20",
+                                         sex="female", password="password")
+        p = Preferences()
+        p.save()
+        s = Settings(dark_theme=False, )
+        s.save()
+        User1.preferences = p
+        User1.settings = s
+        User1.save()
 
-    def test_registration(self):
+    def test_RegistrationView(self):
         factory = APIRequestFactory()
 
         view = RegistrationView.as_view()
@@ -31,9 +52,10 @@ class AuthenticationTestCase(APITestCase):
         request = factory.post('/api/user/register', data)
 
         response = view(request)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_login(self):
+    def test_CustomAuthToken(self):
         factory = APIRequestFactory()
 
         view = obtain_auth_token
@@ -46,18 +68,18 @@ class AuthenticationTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_logout(self):
+    def test_LogoutView(self):
         # TODO: logout unit test
         self.assertEqual(1, 1)
 
-    def test_delete(self):
+    def test_DeleteUserAccountView(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
         view = DeleteUserAccountView.as_view()
 
-        request = factory.delete('/api/user/delete')
+        request = factory.delete('/api/user/delete', {'password': 'password'})
 
         force_authenticate(request, user=user)
 
@@ -65,31 +87,7 @@ class AuthenticationTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-class UserViewTestCase(APITestCase):
-    def setUp(self):
-        User.objects.create_user(username="testcase1", email="testcase1@gmail.com", location="Poznań",
-                                 birthday="1996-06-20",
-                                 sex="female", password="password")
-
-    def test_profile_patch(self):
-        factory = APIRequestFactory()
-
-        user = User.objects.get(username='testcase1')
-
-        view = UserProfileView.as_view()
-
-        data = {'name': 'Mateusz', 'surname': 'Błoszyk', "description": ""}
-
-        request = factory.patch('/api/user/properties', data)
-
-        force_authenticate(request, user=user)
-
-        response = view(request)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_profile_get(self):
+    def test_UserProfileView_GET(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
@@ -101,18 +99,22 @@ class UserViewTestCase(APITestCase):
         force_authenticate(request, user=user)
 
         response = view(request)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_preferences_patch(self):
+    def test_UserProfileView_PATCH(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = PreferencesView.as_view()
+        view = UserProfileView.as_view()
 
-        data = {"orientation": "Male"}
+        data = {
+            'sex': 'Mężczyzna',
+            'hair_color': 'Czarne'
+        }
 
-        request = factory.patch('/api/user/preferences', data)
+        request = factory.patch('/api/user/properties', data)
 
         force_authenticate(request, user=user)
 
@@ -120,7 +122,7 @@ class UserViewTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_preferences_get(self):
+    def test_PreferencesView_GET(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
@@ -132,21 +134,22 @@ class UserViewTestCase(APITestCase):
         force_authenticate(request, user=user)
 
         response = view(request)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_picture_patch(self):
+    def test_PreferencesView_PATCH(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = UserProfilePic.as_view()
+        view = PreferencesView.as_view()
 
-        image = Image.new('RGB', (100, 100))
-        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
-        image.save(tmp_file)
-        tmp_file.seek(0)
+        data = {
+            'hair_color_blonde_preference': '1',
+            'growth_preference': '1'
+        }
 
-        request = factory.patch('/api/user/picture', {'profile_picture': tmp_file}, format='multipart')
+        request = factory.patch('/api/user/preferences', data)
 
         force_authenticate(request, user=user)
 
@@ -154,69 +157,63 @@ class UserViewTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_picture_get(self):
+    def test_SettingsView_GET(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = UserProfilePic.as_view()
+        view = SettingsView.as_view()
 
-        image = Image.new('RGB', (100, 100))
-        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
-        image.save(tmp_file)
-        tmp_file.seek(0)
-        request = factory.patch('/api/user/picture', {'profile_picture': tmp_file}, format='multipart')
+        request = factory.get('/api/user/settings')
+
         force_authenticate(request, user=user)
 
         response = view(request)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-class UsersViewTestCase(APITestCase):
-    def setUp(self):
-        User.objects.create_user(username="testcase1", email="testcase1@gmail.com", location="Poznań",
-                                 birthday="1996-06-20",
-                                 sex="female", password="password")
-        User.objects.create_user(username="testcase2", email="testcase2@gmail.com", location="Warszawa",
-                                 birthday="1991-06-20",
-                                 sex="male", password="password")
-        User.objects.create_user(username="testcase3", email="testcase3@gmail.com", location="Poznań",
-                                 birthday="1970-06-20",
-                                 sex="male", password="password")
-        User.objects.create_user(username="testcase4", email="testcase4@gmail.com", location="Wrocław",
-                                 birthday="1980-06-20",
-                                 sex="male", password="password")
-        User.objects.create_user(username="testcase5", email="testcase5@gmail.com", location="Warszawa",
-                                 birthday="1999-06-20",
-                                 sex="female", password="password")
-
-    def test_detail(self):
+    def test_SettingsView_PATCH(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = user_detail
+        view = SettingsView.as_view()
 
-        data = {}
+        data = {
+            'dark_theme': True,
+        }
 
-        request = factory.get('/api/user/users/3', data)
+        request = factory.patch('/api/user/settings', data)
 
         force_authenticate(request, user=user)
 
-        response = view(request, pk=3)
+        response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list(self):
+    def test_LikesView_user_detail(self):
+        factory = APIRequestFactory()
+
+        user = User.objects.get(username='testcase1')
+
+        view = SettingsView.as_view()
+
+        request = factory.get('/api/user/users/2')
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_UserListView_user_list(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
         view = user_list
 
-        data = {}
-
-        request = factory.get('/api/user/users', data)
+        request = factory.get('/api/user/users')
 
         force_authenticate(request, user=user)
 
@@ -224,16 +221,35 @@ class UsersViewTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list_search(self):
+    def test_create_like(self):
+        factory = APIRequestFactory()
+
+        user1 = User.objects.get(username='testcase1')
+        user2 = User.objects.get(username='testcase2')
+
+        view = create_like
+
+        data = {
+            'pk': user2.pk,
+            'value': 'like'
+        }
+
+        request = factory.post('/api/user/create-like', data)
+
+        force_authenticate(request, user=user1)
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_liked_users(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = user_list
+        view = get_liked_users
 
-        data = {}
-
-        request = factory.get('/api/user/users?search=Mateusz', data)
+        request = factory.get('/api/user/get-users-are-liked/' + str(user.pk))
 
         force_authenticate(request, user=user)
 
@@ -241,16 +257,44 @@ class UsersViewTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list_filter(self):
+    def test_get_like_users(self):
         factory = APIRequestFactory()
 
         user = User.objects.get(username='testcase1')
 
-        view = user_list
+        view = get_like_users
 
-        data = {}
+        request = factory.get('/api/user/get-users-liked/' + str(user.pk))
 
-        request = factory.get('/api/user/users?location=Wrocław&birthday=1980-06-20', data)
+        force_authenticate(request, user=user)
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_liked_user(self):
+        factory = APIRequestFactory()
+
+        user = User.objects.get(username='testcase1')
+
+        view = get_liked_user
+
+        request = factory.get('/api/user/get-user-are-liked/')
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_like_user(self):
+        factory = APIRequestFactory()
+
+        user = User.objects.get(username='testcase1')
+
+        view = get_like_user
+
+        request = factory.get('/api/user/get-user-liked/')
 
         force_authenticate(request, user=user)
 
